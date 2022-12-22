8c8e7412953aaed1a42643d57fd0a876d0fceaba
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2022-12-22 23:50:43 +0300

    Add project files.

---
+++ webHomework/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace webHomework.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
