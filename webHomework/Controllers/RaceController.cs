b4449f5b65dd50394fb476aab7b2c8db72c2e164
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2022-12-26 17:43:42 +0300

    race detail page

--- webHomework/Controllers/RaceController.cs
+++ webHomework/Controllers/RaceController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using webHomework.Data;
 using webHomework.Models;
 
@@ -17,6 +18,11 @@ namespace webHomework.Controllers
         {
             List<Race> races = _context.Races.ToList();
             return View(races);
+        }
+        public IActionResult Detail(int id) 
+        {
+            Race race = _context.Races.Include(a => a.Address).FirstOrDefault(C => C.Id == id);
+            return View(race);
         }
     }
 }
