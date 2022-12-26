6dc22b775a88a2e1ab32ea08dd2c6b2680001b9f
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2022-12-27 00:22:48 +0300

    fixed

--- webHomework/Controllers/RaceController.cs
+++ webHomework/Controllers/RaceController.cs
@@ -1,27 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using webHomework.Data;
+using webHomework.Interfaces;
 using webHomework.Models;
 
 namespace webHomework.Controllers
 {
     public class RaceController : Controller
     {
-        private readonly ApplicationDbContext _context;
-        public RaceController(ApplicationDbContext context)
+       
+        private readonly IRaceRepository _raceRepository;
+
+        public RaceController(IRaceRepository raceRepository)
         {
-            _context = context;
+            
+            _raceRepository = raceRepository;
         }
 
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            List<Race> races = _context.Races.ToList();
+            IEnumerable<Race> races = await _raceRepository.GetAll();
             return View(races);
         }
-        public IActionResult Detail(int id) 
+        public async Task<IActionResult> Detail(int id) 
         {
-            Race race = _context.Races.Include(a => a.Address).FirstOrDefault(C => C.Id == id);
+            Race race = await _raceRepository.GetByIdAsync(id);
             return View(race);
         }
     }
