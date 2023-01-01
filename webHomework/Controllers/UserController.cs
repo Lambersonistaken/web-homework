9490f0b1eb6ff904d2d31c911ca9e57be2c0d35e
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2023-01-01 17:29:45 +0300

    updated

--- webHomework/Controllers/UserController.cs
+++ webHomework/Controllers/UserController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using webHomework.Interfaces;
 
 namespace webHomework.Controllers
 {
     public class UserController : Controller
     {
-        [HttpGet("users")]
-        public IActionResult Index()
+        private readonly IUserRepository _userRepository;
+
+        public UserController(IUserRepository userRepository)
         {
+            _userRepository = userRepository;
+        }
+
+        [HttpGet("users")]
+        public async IActionResult Index()
+        {
+            var users = await _userRepository.GetAllUsers();
             return View();
         }
     }
---
+++ webHomework/Interfaces/IUserRepository.cs
@@ -0,0 +1,21 @@
+﻿using webHomework.Models;
+
+namespace webHomework.Interfaces
+{
+    public interface IUserRepository
+    {
+
+        Task<IEnumerable<AppUser>> GetAllUsers();
+
+        Task<AppUser> GetUserById(string id);
+
+        bool Add(AppUser user);
+
+        bool Update(AppUser user);
+
+        bool Delete(AppUser user);
+
+        bool Save();
+
+    }
+}
